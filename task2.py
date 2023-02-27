18c314ba81e40adcaba4fb447297f5f7c3681829
Docvel <docvel@yandex.ru>
2023-02-27 18:51:05 +0300

    task 2 is solved

--- task2.py
+++ task2.py
@@ -1,4 +1,15 @@
+from checkingInput import correct_int
+
 # Найдите сумму цифр трехзначного числа.
 # Пример:
 # 123 -> 6 (1 + 2 + 3)
 # 100 -> 1 (1 + 0 + 0)
+
+print("Введите любое целое число: ")
+number = correct_int()
+result = 0
+while number >= 1:
+    temp = number % 10
+    result += temp
+    number = number // 10
+print("Результат: ", result)
